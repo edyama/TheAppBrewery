bb4878f7c14f73e245f56dbd8bbc39e0fbebc65d
edyama <ey.yamagat@gmail.com>
2021-08-17 15:46:00 -0300

    FlashChat updated

---
+++ Flash-Chat-iOS13-master/Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Ed Yama on 17/08/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
